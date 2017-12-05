d840268d56b2be1290cc87eb06ec410dee70e062
buzzhaha <boming.fan@gmail.com>
2017-12-05 15:48:55 -0800

    hw6-start

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// make a giphy api key
+
+// make buttons
+
+// make form to add new buttons
+
+// create variables arrays
+
+// create an ajax call
+
+// disect the object (title, rating, images)
+
+// display them to user
+
+// on click - animate gifs, second click, stop animate
