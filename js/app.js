2d2404452467a6159a65a9dfcec6764b1bced168
buzzhaha <boming.fan@gmail.com>
2017-12-07 15:10:13 -0800

    document fix

--- js/app.js
+++ js/app.js
@@ -4,43 +4,61 @@
 var apiKey = "cb29ZcymMTuZXtQxexw9GtY41MXTaXWH"
 // var animals = [dogs, cats];
 
+function showImage(keyWord) {
+    // creating variables grabing user input and queryURL
+    // animal = $("#userInput").val();
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + keyWord + "&limit=10&offset=0&lang=en";
+ 
+     // clear out any previous images on the page when there is a new search happening
+     $("#image").empty();
+ 
+     $.ajax({ url: queryURL, method: 'GET' })
+         .done(function (response) {
+ 
+             console.log(response);
+             for (var i = 0; i < response.data.length; i++) {
+                 // display a list of gifs
+                 var newImg = $("<img>");
+                 newImg.attr("src", response.data[i].images.fixed_height_still.url);
+                 $("#image").append(newImg);
+                 
+             }
+         });
+}
 
+// make a new button for them to click on later
+   function makeButton(input) {
+    var newBtn = $("<button>");
+    newBtn.attr("type", "button");
+    newBtn.addClass("btn btn-primary");
+    newBtn.text(input);
+    newBtn.attr("data-animal", input);
+    $('#addButton').append(newBtn);
+  }
 
-
-
-// onclick function ajax call and display the image 
-// function getImage() {
 
 // targeting button id=search  
 $("#search").on("click", function (event) {
-
     //  Preventing the submit button from trying to submit the form
     event.preventDefault();
-
-    // creating variables grabing user input and queryURL
     var animal = $("#userInput").val();
-    console.log(animal);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&lang=en";
-
-    // clear out any previous images on the page when there is a new search happening
-    $("#image").empty();
-
-    $.ajax({ url: queryURL, method: 'GET' })
-        .done(function (response) {
-
-            console.log(response);
-            for (var i = 0; i < response.data.length; i++) {
-                // display a list of gifs
-                var newImg = $("<img>");
-                newImg.attr("src", response.data[i].images.downsized_still.url);
-                $("#image").append(newImg);
-                
-            }
-        });
+    showImage(animal);
+    makeButton(animal);
 });
-// }
 
 
+// targeting generated button
+// $("button").on("click", function () {  
+//     var dataAnimal = $(this).attr("data-animal");
+//     console.log(dataAnimal);
+//     showImage(dataAnimal);
+// });
+
+
+$(document).on("click", "button", function(event) {
+  showImage($(this).attr("data-animal"));
+});
 
 // finds giphy results based on text
 // from the user input 
